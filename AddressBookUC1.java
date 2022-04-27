0b2f52bb2619c4fe5e13f1bf7272ec5395f7d9b8
GowthamST3 <gowthamgiri3@gmail.com>
2022-04-27 23:38:10 +0530

    Added - Welcome to Address Book Program

---
+++ AddressBookUC1.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz_Day9Assignment;
+
+public class AddressBookUC1 {
+	 
+	public void Wlcome(){
+		System.out.println("Welcome to Address Book Program ");
+	}
+
+	public static void main(String[] args) {
+		AddressBookUC1 	addressBookUC1=new AddressBookUC1();
+		addressBookUC1.Wlcome();
+   }
+}
